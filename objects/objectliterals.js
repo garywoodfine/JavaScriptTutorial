4f3de67155d880a2cf7ad43ad059d5285cc3c845
gary <gary.woodfine@threenine.co.uk>
2015-11-25 21:29:57 +0000

    Added Object creation examples

---
+++ objects/objectliterals.js
@@ -0,0 +1,22 @@
+
+var myemptyobject = {};        // An empty object
+
+var mynameobject = {"FirstName" : "Gary", "Lastname" : "Woodfine"};   // object with 2 properties
+
+
+// A complex object containing a another object
+var myblogobject = {
+    "title" : "Javascript Object" ,
+    "Content" : "Blah Blah ",
+    author :{
+        "FirstName" : "Gary",
+        "Lastname" : "Woodfine"
+    }
+}
+
+
+// Core  constructor objects
+var object = new Object();              // Create an empty object i.e. {}
+var date = new Date();                  // Create a Date Object
+var array = new Array();                // Create an empty array i.e. []
+var regex = new RegExp("js");           // Create a Regular Expression object
